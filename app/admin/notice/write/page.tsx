ada439eae701ff84127402503146b25c9844dcb5
j3261221 <j3261221@gmail.com>
2024-02-28 16:38:11 +0900

    first commit

--- app/admin/notice/write/page.tsx
+++ app/admin/notice/write/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState, ChangeEvent, FormEvent } from "react";
 import "../../../../styles/admin.scss";
 
--- app/auth/signIn/page.tsx
+++ app/auth/signIn/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { signIn } from "next-auth/react";
 import { useRef, useState } from 'react';
 import "../../../styles/auth.css";
